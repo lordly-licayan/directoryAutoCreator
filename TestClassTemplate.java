408af4be177e65ea2dc6441164377fe53730f6e6
lordly <lordly.licayan@gmail.com>
2019-10-22 14:37:15 +0800

    added new info

--- TestClassTemplate.java
+++ TestClassTemplate.java
@@ -5,13 +5,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test<ClassName> extends TestSQLInjection {
 	public Test<ClassName>() {
 		super();
 	}
 
+	private static final Map<String, String> testInfo = new HashMap<String, String>() {
+		{
+			put("Asset", "<targetAsset>");
+			put("Line No.", "<targetLineNo>");
+			put("Findings", "<targetFindings>");
+			put("TestClass", Test<ClassName>.class.getCanonicalName());
+		}
+	};
+	
 	public static void main(String[] args) throws Throwable {
 		Test<ClassName> klaz = new Test<ClassName>();
 	}
